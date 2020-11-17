74fd4b3d6347cc675dd7e1ad9e3e32fe16e3f292
stacksonstack <staceycarrillo44@gmail.com>
2020-11-17 09:43:29 -0500

    pulled apiresponse to dogContainer and started added props

--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import apiResponse from '../api';
+import DogCard from '../Components/DogCard'
 
 class DogsList extends Component {
+  
+  
+
+  renderDogs = () => {
+    apiResponse.map((dogObj) => {
+      const dogName = dogObj.name
+      const dogImg = dogObj.img
+      const dogBreed = dogObj.breed
+      const dogId = dogObj.id
+
+    })
+  }
+  
   render() {
-    return <div className="list">{/*list of dogs go here*/}</div>;
+    return <div className="list">
+      <DogCard name={this.renderDogs.dogName} img={this.renderDogs.dogImg} breed={this.renderDogs.dogBreed} key={this.renderDogs.dogId}/>
+    </div>;
   }
 }
 
